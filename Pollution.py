051b6cc32a2e36d9aac5f1b9a4e34afb3910cec7
Michael Angino <mfa7@rice.edu>
2020-06-06 17:37:26 -0400

    Implemented Predict_point function

--- Pollution.py
+++ Pollution.py
@@ -1,5 +1,7 @@
-import numpy
-
+import numpy as np
+# import sklearn as sk
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 
 class Point:
     """
@@ -38,24 +40,71 @@ class Point:
         return self.x
 
     def get_position(self):
+        """
+        Returns Position of the point
+        :return: list [x,y]
+        """
         return [self.x,self.y]
 
 
-
-
-def create_list_of_random_pollution_points(length, mean, std):
+def create_map_of_random_pollution_points(length, mean, std):
     """
-    Creates a list of points with random Pollution Values using a gaussian distribution in 1D evenly starting at x = 05 every 10
+    Creates a map of points with random Pollution Values using a gaussian distribution in 1D evenly starting at x = 05 every 10
     :param length: Wanted length of list
     :param mean: Mean of gaussian distribution of pollution values
     :param std: Standard Deviation of gaussian distribution of pollution values
-    :return: list of points
+    :return: map of points with key being their label and the value being a point object
     """
-    new_list = []
+    new_map = {}
     x = 5
     for i in range(0,100):
-        new_list.append(Point(i,numpy.random.normal(mean,std),x))
+        new_map[i] = (Point(i,np.random.normal(mean,std),x))
         x = x+10
-    return new_list
+    return new_map
+
+def predict_point(known_points, wanted_point_positions):
+    """
+    Predicts points based on known data using Kriging (Gaussian Processes)
+    :param known_points: list of points
+    :param wanted_point_positions: list of wanted point posistions [[x1,y1], [x2,y2]]
+    :return: a list of all predicted pollution values
+    """
+
+
+    gp = GaussianProcessRegressor(C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))) # Instantiate a Gaussian Process model
+
+
+    known_points_position_list = []  #converts point list into a list of [x,y] values
+    for point in known_points:
+        known_points_position_list.append(point.get_position())
+
+    pollution_value_list = [] #converts point list into a list of pollution values
+    for point in known_points:
+        pollution_value_list.append(point.get_pollution_value())
+
+    gp.fit(known_points_position_list,pollution_value_list) #Fits model to data
+
+
+    prediction_list = known_points_position_list
+    prediction_list.extend(wanted_point_positions)
+    return gp.predict(prediction_list)[len(known_points):] # predicts on new data
+
+
+
+
+
+
+
+
+
+random_points = create_map_of_random_pollution_points(100,100,10).values()
+test = []
+for i in random_points:
+    test.append(i.get_pollution_value())
+
+testNP = np.array(test)
+prediction = predict_point(random_points,[[100,0],[3,0]])
+prediction_list = prediction.tolist()
+print(prediction_list)
 
 
