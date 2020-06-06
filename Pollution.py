ef494da48ad37a8d592cc76e96d2af4e74bade65
Michael Angino <mfa7@rice.edu>
2020-06-06 14:48:57 -0400

    Implemented Point Class and Random Point List Method

--- Pollution.py
+++ Pollution.py
@@ -1,2 +1,61 @@
+import numpy
 
-# test
+
+class Point:
+    """
+    Object representing a point in Space
+    """
+
+
+    def __init__(self, label, actual_pollution_value, x):
+        """
+        Creates a Point Object
+        :param label: Label of point ex:005, 015, 995
+        :param actual_pollution_value: Actual value of the pollution measurment
+        """
+        self.label = label
+        # self.actual_pollution_value = actual_pollution_value
+        self.pollution_value = actual_pollution_value
+
+        self.x = x
+
+        self.y = 0
+
+
+    def __str__(self):
+        return "[Point: " + str(self.label) + "| APV: " + str(self.pollution_value)+"| Position: {" + str(self.x)+ "," + str(self.y)+ "} ]"
+
+    def get_pollution_value(self):
+        return self.pollution_value
+
+    def set_pollution_value(self, new_pollution_value):
+        self.pollution_value = new_pollution_value
+
+    def get_label(self):
+        return self.label
+
+    def get_x_cord(self):
+        return self.x
+
+    def get_position(self):
+        return [self.x,self.y]
+
+
+
+
+def create_list_of_random_pollution_points(length, mean, std):
+    """
+    Creates a list of points with random Pollution Values using a gaussian distribution in 1D evenly starting at x = 05 every 10
+    :param length: Wanted length of list
+    :param mean: Mean of gaussian distribution of pollution values
+    :param std: Standard Deviation of gaussian distribution of pollution values
+    :return: list of points
+    """
+    new_list = []
+    x = 5
+    for i in range(0,100):
+        new_list.append(Point(i,numpy.random.normal(mean,std),x))
+        x = x+10
+    return new_list
+
+
---
+++ Pollution_test.py
@@ -0,0 +1,35 @@
+import unittest
+
+import Pollution
+
+
+class MyTestCase(unittest.TestCase):
+    def test_point_list(self):
+        """
+        Testing Create_list_of_points method
+        :return:
+        """
+        test_list = Pollution.create_list_of_random_pollution_points(100,100,0)
+        self.assertEqual(100, len(test_list))
+        for i in range(0,len(test_list)):
+            self.assertEqual(100,test_list[i].get_pollution_value())
+
+        self.assertEqual(5, test_list[0].get_x_cord())
+        self.assertEqual(995, test_list[99].get_x_cord())
+
+    def test_point(self):
+        """
+        Testing Point Class
+        :return:
+        """
+        test_point = Pollution.Point(1,100,0)
+        self.assertEqual(1,test_point.get_label())
+        self.assertEqual(100,test_point.get_pollution_value())
+        test_point.set_pollution_value(20)
+
+        self.assertEqual(20, test_point.get_pollution_value())
+
+
+
+
+
