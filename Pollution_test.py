2cade04b09fffc5ae03eb8e6617b914627149e9c
Ashay Swadi <ashay.swadi@gmail.com>
2020-06-08 14:15:22 -0500

    added test for RMSE function

--- Pollution_test.py
+++ Pollution_test.py
@@ -9,10 +9,10 @@ class MyTestCase(unittest.TestCase):
         Testing Create_list_of_points method
         :return:
         """
-        test_list = Pollution.create_points_with_random_pollution(100,100,0)
+        test_list = Pollution.create_points_with_random_pollution(100, 100, 0)
         self.assertEqual(100, len(test_list))
-        for i in range(0,len(test_list)):
-            self.assertEqual(100,test_list[i].get_actual_pollution_value())
+        for i in range(0, len(test_list)):
+            self.assertEqual(100, test_list[i].get_actual_pollution_value())
 
         self.assertEqual(5, test_list[0].get_x_cord())
         self.assertEqual(995, test_list[99].get_x_cord())
@@ -22,9 +22,9 @@ class MyTestCase(unittest.TestCase):
         Testing Point Class
         :return:
         """
-        test_point = Pollution.Point(1,100,0)
-        self.assertEqual(1,test_point.get_label())
-        self.assertEqual(100,test_point.get_actual_pollution_value())
+        test_point = Pollution.Point(1, 100, 0)
+        self.assertEqual(1, test_point.get_label())
+        self.assertEqual(100, test_point.get_actual_pollution_value())
         test_point.set_pollution_value(20)
 
         self.assertEqual(20, test_point.get_pollution_value())
@@ -35,20 +35,20 @@ class MyTestCase(unittest.TestCase):
         :return:
         """
 
-        test_points = Pollution.create_points_with_random_pollution(10,10,1)
+        test_points = Pollution.create_points_with_random_pollution(10, 10, 1)
         counter = defaultdict(int)
         num_of_runs = 10000
 
-        for i in range(0,num_of_runs):
+        for i in range(0, num_of_runs):
             a = Pollution.pick_uniform_random_points(test_points, 5)
             for key in a.keys():
-                counter[key] = counter[key]+1
+                counter[key] = counter[key] + 1
 
         for key in counter.keys():
-            counter[key] = counter[key]/ num_of_runs
+            counter[key] = counter[key] / num_of_runs
 
-        for i in range(0,9):
-            self.assertAlmostEqual(.5, counter[i],None,None,.02)
+        for i in range(0, 9):
+            self.assertAlmostEqual(.5, counter[i], None, None, .02)
 
     def test_interpolation_mutation(self):
         """
@@ -56,19 +56,22 @@ class MyTestCase(unittest.TestCase):
         :return:
         """
 
-        points = Pollution.create_points_with_random_pollution(100,100,10)
+        points = Pollution.create_points_with_random_pollution(100, 100, 10)
 
-        picked_points = Pollution.pick_uniform_random_points(points,50)
+        picked_points = Pollution.pick_uniform_random_points(points, 50)
 
-        interpolated_points = Pollution.interpolate_unknown_points(picked_points,points)
+        interpolated_points = Pollution.interpolate_unknown_points(picked_points, points)
 
         for label, point in picked_points.items():
-            self.assertEqual(picked_points[label].get_pollution_value(),interpolated_points[label].get_pollution_value())
+            self.assertEqual(picked_points[label].get_pollution_value(),
+                             interpolated_points[label].get_pollution_value())
 
-        for label,point in points.items():
+        for label, point in points.items():
             self.assertEqual(points[label].get_position(), interpolated_points[label].get_position())
-            self.assertEqual(points[label].get_actual_pollution_value(), interpolated_points[label].get_actual_pollution_value(),
-                             "test failed 1 APV:" + str(points[label].get_actual_pollution_value()) + "| 2 APV: " + str(interpolated_points[label].get_actual_pollution_value()))
+            self.assertEqual(points[label].get_actual_pollution_value(),
+                             interpolated_points[label].get_actual_pollution_value(),
+                             "test failed 1 APV:" + str(points[label].get_actual_pollution_value()) + "| 2 APV: " + str(
+                                 interpolated_points[label].get_actual_pollution_value()))
 
     def test_rmse_calculation(self):
         """
@@ -76,8 +79,16 @@ class MyTestCase(unittest.TestCase):
         :return:
         """
         test_points = Pollution.create_points_with_random_pollution(100, 100, 0)
-        test_picked_points = Pollution.pick_uniform_random_points(test_points, 10)
+        test_picked_points = Pollution.pick_uniform_random_points(test_points, 100)
         test_interpolated_points = Pollution.interpolate_unknown_points(test_picked_points, test_points)
         self.assertEqual(100, len(test_points))
         for i in range(0, len(test_points)):
             self.assertEqual(100, test_points[i].get_actual_pollution_value())
+        for label, point in test_picked_points.items():
+            self.assertEqual(test_picked_points[label].get_pollution_value(),
+                             test_interpolated_points[label].get_pollution_value())
+        for label, point in test_points.items():
+            self.assertEqual(test_points[label].get_actual_pollution_value(),
+                             test_interpolated_points[label].get_actual_pollution_value())
+        test_rmse = Pollution.root_mean_square_error(test_interpolated_points)
+        self.assertEqual(0.0, test_rmse)
